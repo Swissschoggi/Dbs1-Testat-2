06929f3785c039d822b13105c824b232a63fc2c9
swissschoggi <fynn.gaechter@gmail.com>
2025-11-10 14:06:17 +0100

    Added Subquery and ANY query

--- 5_queries.sql
+++ 5_queries.sql
@@ -1,2 +1,6 @@
 SELECT DISTINCT typ FROM auto;
+
 -- DISTINCT gibt alle autotypen aus ohne duplikate.
+
+  SELECT gewicht, fahrzeugid
+FROM auto WHERE fahrzeugid = ANY (SELECT fahrzeugid FROM ausleihe WHERE gewicht > 1500);
